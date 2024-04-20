784d78956112e3d559cd8e26a75d4faf84df3895
Darlan0307 <darlanchagas2020@gmail.com>
2024-04-20 10:49:51 -0300

    :sparkles:feat: usando componente para mover a pagina para o top

--- src/components/pages/SignUp/index.tsx
+++ src/components/pages/SignUp/index.tsx
@@ -16,6 +16,7 @@ import {
   signInGithubPopup,
   signInGithubRedirect,
 } from "@/firebase/auth/authGithub";
+import MoveToTop from "@/utils/MoveToTop";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -45,114 +46,117 @@ const SignUp = () => {
   };
 
   return (
-    <main className="w-full max-w-[400px] mx-auto flex flex-col gap-6 px-4">
-      <h1 className="text-3xl text-center font-bold">SignUp</h1>
+    <>
+      <MoveToTop />
+      <main className="w-full max-w-[400px] mx-auto flex flex-col gap-6 px-4">
+        <h1 className="text-3xl text-center font-bold">SignUp</h1>
 
-      <form
-        onSubmit={handleSubmit}
-        autoComplete="off"
-        className="flex flex-col items-center gap-8"
-      >
-        <div className="w-full relative">
-          <Input
-            id="email"
-            className="peer text-lg"
-            required
-            min={1}
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-          <Label
-            htmlFor="email"
-            className="absolute top-[10%] left-[5%] transition-all text-lg  peer-focus-within:top-[-70%] peer-focus-within:left-1 peer-focus-within:text-primary peer-valid:top-[-70%] peer-valid:left-1 peer-valid:text-primary "
-          >
-            Email
-          </Label>
+        <form
+          onSubmit={handleSubmit}
+          autoComplete="off"
+          className="flex flex-col items-center gap-8"
+        >
+          <div className="w-full relative">
+            <Input
+              id="email"
+              className="peer text-lg"
+              required
+              min={1}
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <Label
+              htmlFor="email"
+              className="absolute top-[10%] left-[5%] transition-all text-lg  peer-focus-within:top-[-70%] peer-focus-within:left-1 peer-focus-within:text-primary peer-valid:top-[-70%] peer-valid:left-1 peer-valid:text-primary "
+            >
+              Email
+            </Label>
+          </div>
+          <div className="w-full relative">
+            <Input
+              id="password"
+              className="peer text-lg"
+              required
+              min={1}
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Label
+              htmlFor="password"
+              className="absolute top-[10%] left-[5%] transition-all text-lg  peer-focus-within:top-[-70%] peer-focus-within:left-1 peer-focus-within:text-primary peer-valid:top-[-70%] peer-valid:left-1 peer-valid:text-primary "
+            >
+              Password
+            </Label>
+          </div>
+          <div className="w-full relative">
+            <Input
+              id="confirmpassword"
+              className="peer text-lg"
+              required
+              min={1}
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <Label
+              htmlFor="confirmpassword"
+              className="absolute top-[10%] left-[5%] transition-all text-lg  peer-focus-within:top-[-70%] peer-focus-within:left-1 peer-focus-within:text-primary peer-valid:top-[-70%] peer-valid:left-1 peer-valid:text-primary "
+            >
+              Confirm Password
+            </Label>
+          </div>
+          <Button type="submit" className="w-full text-white text-xl">
+            register
+          </Button>
+        </form>
+
+        <div className="relative my-3">
+          <div className="h-[1px] w-full bg-black dark:bg-white"></div>
+          <span className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-background font-bold text-xl p-1">
+            OR
+          </span>
         </div>
-        <div className="w-full relative">
-          <Input
-            id="password"
-            className="peer text-lg"
-            required
-            min={1}
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Label
-            htmlFor="password"
-            className="absolute top-[10%] left-[5%] transition-all text-lg  peer-focus-within:top-[-70%] peer-focus-within:left-1 peer-focus-within:text-primary peer-valid:top-[-70%] peer-valid:left-1 peer-valid:text-primary "
-          >
-            Password
-          </Label>
-        </div>
-        <div className="w-full relative">
-          <Input
-            id="confirmpassword"
-            className="peer text-lg"
-            required
-            min={1}
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <Label
-            htmlFor="confirmpassword"
-            className="absolute top-[10%] left-[5%] transition-all text-lg  peer-focus-within:top-[-70%] peer-focus-within:left-1 peer-focus-within:text-primary peer-valid:top-[-70%] peer-valid:left-1 peer-valid:text-primary "
-          >
-            Confirm Password
-          </Label>
-        </div>
-        <Button type="submit" className="w-full text-white text-xl">
-          register
+
+        <Button
+          variant="secondary"
+          className="flex items-center gap-2 py-5"
+          onClick={() => {
+            if (window.innerWidth <= 800) {
+              signInGoogleRedirect();
+            } else {
+              signInGooglePopup();
+            }
+            navigation("/");
+          }}
+        >
+          {" "}
+          <FaGoogle size={25} /> <span>Google</span>
         </Button>
-      </form>
+        <Button
+          variant="secondary"
+          className="flex items-center gap-2 py-5"
+          onClick={() => {
+            if (window.innerWidth <= 800) {
+              signInGithubRedirect();
+            } else {
+              signInGithubPopup();
+            }
+            navigation("/");
+          }}
+        >
+          {" "}
+          <FaGithub size={25} /> <span>GitHub</span>
+        </Button>
 
-      <div className="relative my-3">
-        <div className="h-[1px] w-full bg-black dark:bg-white"></div>
-        <span className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-background font-bold text-xl p-1">
-          OR
-        </span>
-      </div>
-
-      <Button
-        variant="secondary"
-        className="flex items-center gap-2 py-5"
-        onClick={() => {
-          if (window.innerWidth <= 800) {
-            signInGoogleRedirect();
-          } else {
-            signInGooglePopup();
-          }
-          navigation("/");
-        }}
-      >
-        {" "}
-        <FaGoogle size={25} /> <span>Google</span>
-      </Button>
-      <Button
-        variant="secondary"
-        className="flex items-center gap-2 py-5"
-        onClick={() => {
-          if (window.innerWidth <= 800) {
-            signInGithubRedirect();
-          } else {
-            signInGithubPopup();
-          }
-          navigation("/");
-        }}
-      >
-        {" "}
-        <FaGithub size={25} /> <span>GitHub</span>
-      </Button>
-
-      <p className="text-center text-lg">
-        Already have an account?{" "}
-        <Link to="/signin" className="text-primary underline">
-          login
-        </Link>
-      </p>
-    </main>
+        <p className="text-center text-lg">
+          Already have an account?{" "}
+          <Link to="/signin" className="text-primary underline">
+            login
+          </Link>
+        </p>
+      </main>
+    </>
   );
 };
 
--- src/components/pages/SingleProduct/index.tsx
+++ src/components/pages/SingleProduct/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useMovie } from "@/context/MovieProvider";
 import { useAuthContext } from "@/context/UserProvider";
+import MoveToTop from "@/utils/MoveToTop";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -24,43 +25,46 @@ const SingleProduct = () => {
   };
 
   return (
-    <main className="relative p-5 flex flex-wrap justify-center items-center gap-6 sm:gap-16">
-      <img
-        src={`https://image.tmdb.org/t/p/w300${movieFiltred?.poster_path}`}
-        alt={movieFiltred?.original_title}
-        className="rounded-xl sm:w-[400px]"
-      />
-      <article className="flex flex-col items-start gap-4 max-w-[400px]">
-        <Button size="lg" className="text-white" onClick={handleClick}>
-          Add Favorites
-        </Button>
-        <h1 className="text-2xl font-bold">{movieFiltred?.title}</h1>
-        <div className="flex flex-col gap-4 ">
-          <p className="text-lg">
-            <span className="text-xl pr-2 font-bold">Popularity:</span>
-            {movieFiltred?.popularity}
+    <>
+      <MoveToTop />
+      <main className="relative p-5 flex flex-wrap justify-center items-center gap-6 sm:gap-16">
+        <img
+          src={`https://image.tmdb.org/t/p/w300${movieFiltred?.poster_path}`}
+          alt={movieFiltred?.original_title}
+          className="rounded-xl sm:w-[400px]"
+        />
+        <article className="flex flex-col items-start gap-4 max-w-[400px]">
+          <Button size="lg" className="text-white" onClick={handleClick}>
+            Add Favorites
+          </Button>
+          <h1 className="text-2xl font-bold">{movieFiltred?.title}</h1>
+          <div className="flex flex-col gap-4 ">
+            <p className="text-lg">
+              <span className="text-xl pr-2 font-bold">Popularity:</span>
+              {movieFiltred?.popularity}
+            </p>
+            <p className="text-lg">
+              <span className="text-xl pr-2 font-bold">Vote Average:</span>
+              {movieFiltred?.vote_average.toFixed(2)}
+            </p>
+            <p className="text-lg">
+              <span className="text-xl pr-2 font-bold">Vote Count:</span>
+              {movieFiltred?.vote_count}
+            </p>
+          </div>
+          <p className="text-lg overflow-y-auto">
+            <span className="text-xl pr-2 font-bold">Overview:</span>
+            {movieFiltred?.overview}
           </p>
-          <p className="text-lg">
-            <span className="text-xl pr-2 font-bold">Vote Average:</span>
-            {movieFiltred?.vote_average.toFixed(2)}
-          </p>
-          <p className="text-lg">
-            <span className="text-xl pr-2 font-bold">Vote Count:</span>
-            {movieFiltred?.vote_count}
-          </p>
-        </div>
-        <p className="text-lg overflow-y-auto">
-          <span className="text-xl pr-2 font-bold">Overview:</span>
-          {movieFiltred?.overview}
-        </p>
-      </article>
+        </article>
 
-      <img
-        src={`https://image.tmdb.org/t/p/w300${movieFiltred?.backdrop_path}`}
-        alt={movieFiltred?.original_title}
-        className="opacity-20 w-full h-full -z-10 absolute top-0 left-0"
-      />
-    </main>
+        <img
+          src={`https://image.tmdb.org/t/p/w300${movieFiltred?.backdrop_path}`}
+          alt={movieFiltred?.original_title}
+          className="opacity-20 w-full h-full -z-10 absolute top-0 left-0"
+        />
+      </main>
+    </>
   );
 };
 
